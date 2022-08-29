4c6db4d6b503a97726982c2f7b2a869919ad3d8d
Yannick Huggler <yannick.huggler@polypoint.ch>
2022-08-29 17:03:49 +0200

    :sparkles: Start working on score_exams.pl script

--- lib/Terminal/Util.pm
+++ lib/Terminal/Util.pm
@@ -6,7 +6,7 @@ use warnings;
 use experimental 'signatures';
 
 use Exporter 'import';
-our @EXPORT = ('prompt_if_file_should_be_opened_in_editor', 'print_usage_for_create_exam_script');
+our @EXPORT = ('prompt_if_file_should_be_opened_in_editor', 'print_usage_for_create_exam_script', 'print_usage_for_score_exams_script');
 
 sub prompt_if_file_should_be_opened_in_editor($file_name) {
     print "Would you like to open the file in vim? (y/n) [n]: ";
@@ -19,10 +19,19 @@ sub prompt_if_file_should_be_opened_in_editor($file_name) {
 }
 
 sub print_usage_for_create_exam_script() {
-    say "A command line utitlity for randomizing answers in a set of questions.";
+    say "A command line utility for randomizing answers in a set of questions.";
     say "";
     say "Usage";
     say "  \$ ./create_exam.pl <PATH_TO_MASTER_EXAM_FILE>";
     say "Examples";
     say "  ./create_exam.pl _short_exam_master_file.txt"
+}
+
+sub print_usage_for_score_exams_script() {
+    say "A command line utility for scoring and evaluating completed exams according to a masterfile containing the correct answers.";
+    say "";
+    say "Usage";
+    say "  \$ ./score_exams.pl <PATH_TO_MASTER_EXAM_FILE> <GLOBBING_PATTERN_TO_EXAM_FILES>";
+    say "Examples";
+    say "  ./score_exams.pl _short_exam_master_file.txt *completed_exam.txt"
 }
--- score_exams.pl
+++ score_exams.pl
@@ -1,6 +1,25 @@
+#! /usr/bin/env perl
+
 use v5.30.3;
 use strict;
 use warnings;
 
+use File::Util;
+use Exam::Parser;
+use Terminal::Util;
+use Data::Dump 'dump';
 
+my $master_file_name = $ARGV[0] || "";
+my $exam_files_globbing_pattern = $ARGV[1] || "";
 
+if ($master_file_name eq "" || $master_file_name eq "help" || $exam_files_globbing_pattern eq "") {
+    print_usage_for_score_exams_script();
+    exit(1);
+}
+
+my $master_file_content = load_file($master_file_name);
+my $exam = parse_exam($master_file_content);
+
+my @files = glob($exam_files_globbing_pattern);
+
+dump(@files);
