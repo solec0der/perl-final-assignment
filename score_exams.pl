26c2744d1e5777214fe14890c4605cf25ae519ff
Yannick Huggler <yannick.huggler@polypoint.ch>
2022-08-29 17:39:15 +0200

    :sparkles: Implement globbing handling for student exam files

--- score_exams.pl
+++ score_exams.pl
@@ -8,18 +8,27 @@ use File::Util;
 use Exam::Parser;
 use Terminal::Util;
 use Data::Dump 'dump';
+use File::Glob ':glob';
 
 my $master_file_name = $ARGV[0] || "";
-my $exam_files_globbing_pattern = $ARGV[1] || "";
+my @student_exam_file_names;
 
-if ($master_file_name eq "" || $master_file_name eq "help" || $exam_files_globbing_pattern eq "") {
-    print_usage_for_score_exams_script();
-    exit(1);
+foreach my $i (1..$#ARGV) {
+  push(@student_exam_file_names, $ARGV[$i]);
+}
+
+if ($master_file_name eq "" || $master_file_name eq "help" || $#student_exam_file_names == 0) {
+  print_usage_for_score_exams_script();
+  exit(1);
 }
 
 my $master_file_content = load_file($master_file_name);
-my $exam = parse_exam($master_file_content);
+my $master_exam = parse_exam($master_file_content);
 
-my @files = glob($exam_files_globbing_pattern);
+my @student_exams;
 
-dump(@files);
+for my $student_exam_file_name (@student_exam_file_names) {
+  my $file_content = load_file($student_exam_file_name);
+  push(@student_exams, parse_exam($file_content));
+}
+
